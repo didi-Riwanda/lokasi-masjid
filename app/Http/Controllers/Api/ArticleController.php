924319fca6a21667bf88d18c5d793134a0f4cba3
masjid al hijrah <masjidalhijrah@192.168.0.110>
2023-10-30 14:06:02 +0700

    Update

--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -6,11 +6,12 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\ArticleResource;
 use App\Models\Article;
 use App\Models\ArticleViewer;
+use FaithFM\SmartSearch\SmartSearch;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         $fields = [
             'id',
@@ -20,7 +21,19 @@ class ArticleController extends Controller
             'imgsrc',
             'created_at',
         ];
-        $model = Article::select($fields)->latest();
+        $pattern = '/[a-zA-Z0-9 , .]/m';
+        $search = $request->q ?? $request->search;
+        $coditional = ! empty($search) && preg_match($pattern, $search);
+
+        $model = Article::select($fields);
+        $model = $model->when($coditional, function($model) use ($search) {
+            $search = new SmartSearch($search, 'title|content');
+            $model->where($search->getBuilderFilter());
+        });
+        $model = $model->when(! empty($request->category), function ($model) use ($request) {
+            $model->where('category_id', $request->category);
+        });
+        $model = $model->latest();
         return ArticleResource::make($model->cursorPaginate(20));
     }
 
