6d7d681d98494ec89ba20c80af7b0fcf314c955e
Reynaldi Aznan <reynaldiaznan450@gmail.com>
2023-12-07 15:40:08 +0700

    Update

--- routes/web.php
+++ routes/web.php
@@ -108,13 +108,3 @@ Route::prefix('document')->name('document.')->group(function () {
 //         }
 //     }
 // });
-
-Route::get('/test', function () {
-    $path = 'app/murottals/mishari-rashid-al-afasy/bisxB04sSAZSCHc8nPbeGCo04jfyB4BHFsbc7qHj.mp3';
-
-    dd(
-        storage_path($path),
-        Storage::fileExists('murottals/mishari-rashid-al-afasy/bisxB04sSAZSCHc8nPbeGCo04jfyB4BHFsbc7qHj.mp3'),
-        file_exists(storage_path($path)),
-    );
-});
