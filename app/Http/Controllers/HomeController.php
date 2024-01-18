fc8a9801cd694e2d145c8c95345b2c0b3d5575a2
Reynaldi Aznan <reynaldiaznan450@gmail.com>
2024-01-18 11:08:38 +0700

    Update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,25 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Alaouy\Youtube\Facades\Youtube;
-use App\Models\Study;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index(Request $request)
     {
-        $kajian = Study::all();
-        foreach ($kajian as $row) {
-            if (preg_match('/.*\?v=(.+?)($|[\&])/m', $row->url, $matches))  {
-                $id = $matches[1];
-                $info = Youtube::getVideoInfo($id);
-
-                $row->thumbnails = json_encode($info->snippet->thumbnails);
-                $row->save();
-            }
-        }
-
         return view('index');
     }
 }
