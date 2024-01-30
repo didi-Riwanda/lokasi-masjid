93326ffe02e904a3813d1bb876e7f3091a58ef97
Reynaldi Aznan <reynaldiaznan450@gmail.com>
2024-01-30 17:41:11 +0700

    Update

--- routes/web.php
+++ routes/web.php
@@ -35,7 +35,7 @@ use Illuminate\Support\Facades\Storage;
 */
 
 Route::get('/testing', function () {
-    $studies = Study::whereNull('thumbnails')->get();
+    $studies = Study::all();
     foreach ($studies as $study) {
         $vid = Youtube::parseVidFromURL($study->url);
         $info = Youtube::getVideoInfo($vid);
